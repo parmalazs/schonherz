29b356005d03a919319b4882ba53714b9c28eb46
Gyöngyösi Tamás <gytomi@gmail.com>
2013-05-07 18:14:45 +0200

    jobsActivity

--- FlottaKezelo/src/com/schonherz/flottadroid/MainActivity.java
+++ FlottaKezelo/src/com/schonherz/flottadroid/MainActivity.java
@@ -2,10 +2,16 @@ package com.schonherz.flottadroid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	Button jobsButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         Log.i("proba","pusholashoz");
+        
+        jobsButton=(Button) findViewById(R.id.buttonJobs);
+        jobsButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this, JobsActivity.class);
+				MainActivity.this.startActivity(intent);
+			}
+		});
         
     }
 
