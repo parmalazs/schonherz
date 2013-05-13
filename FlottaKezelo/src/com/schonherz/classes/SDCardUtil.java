0d7a57b5c86a0b78dab71fbf5d826b6c7079c764
Gyöngyösi Tamás <gytomi@gmail.com>
2013-05-13 21:10:26 +0200

    SD card util class

---
+++ FlottaKezelo/src/com/schonherz/classes/SDCardUtil.java
@@ -0,0 +1,39 @@
+package com.schonherz.classes;
+
+import android.os.Environment;
+import android.os.StatFs;
+
+public class SDCardUtil {
+	
+	public static Boolean checkAvailable() {
+		String state=Environment.getExternalStorageState();
+		if(Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static Boolean checkIsReadOnly () {
+		String state=Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+			return true;
+		} 
+		else {
+			return false;
+		}
+	}
+	
+	public static int getAvailableSpaceInMegaBytes() {
+		int availableSpace=0;
+		try {
+            StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
+            availableSpace =stat.getAvailableBlocks() * stat.getBlockSize() / 1048576;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return availableSpace;
+	}
+
+}
