2731d8e2f8cb497e156866faead1ada7af729812
Gyöngyösi Tamás <gytomi@gmail.com>
2013-05-15 21:19:43 +0200

    networkUtil update: isWifi, isMobile

--- FlottaKezelo/src/com/schonherz/classes/NetworkUtil.java
+++ FlottaKezelo/src/com/schonherz/classes/NetworkUtil.java
@@ -20,5 +20,27 @@ public class NetworkUtil {
 		return false;
 
 	}
+	
+	public static boolean checkWifiIsActive(Context context) {
+		ConnectivityManager connec = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		android.net.NetworkInfo wifi = connec
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (wifi.isConnected()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean checkMobileIsActive(Context context) {
+		ConnectivityManager connec = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		android.net.NetworkInfo mobile = connec
+				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		if (mobile.isConnected()) {
+			return true;
+		}
+		return false;
+	}
 
 }
