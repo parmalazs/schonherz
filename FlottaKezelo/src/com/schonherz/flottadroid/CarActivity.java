4e3889c66f47c2835af1845cc2408b7fd0e188b1
Tivadar <Tivadar@152.66.215.100>
2013-05-25 01:05:32 +0200

    Autok activity (még nem teljes)

--- FlottaKezelo/src/com/schonherz/flottadroid/CarActivity.java
+++ FlottaKezelo/src/com/schonherz/flottadroid/CarActivity.java
@@ -1,20 +1,122 @@
 package com.schonherz.flottadroid;
 
+import java.util.ArrayList;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
-public class CarActivity extends Activity {
+import com.schonherz.adapters.AutoAdapter;
+import com.schonherz.classes.PullToRefreshListView;
+import com.schonherz.dbentities.Auto;
+import com.schonherz.dbentities.AutoDao;
+import com.schonherz.dbentities.AutoKepDao;
+import com.schonherz.dbentities.DaoMaster;
+import com.schonherz.dbentities.DaoMaster.DevOpenHelper;
+import com.schonherz.dbentities.DaoSession;
+import com.schonherz.flottadroid.JobsActivity.JobsPagerAdapter;
+import com.schonherz.fragments.AutoListFragment;
+import com.schonherz.fragments.SzabadAutoListFragment;
+import com.schonherz.fragments.SzabadMunkaListFragment;
 
+public class CarActivity extends FragmentActivity implements ActionBar.TabListener {
+
+	Context context;
+	AutoDao autoDao;
+	AutoKepDao autoKepDao;
+	AutoAdapter adapter;
+	PullToRefreshListView pullListView;
+	ArrayList<Auto> autok;
+	MenuItem refreshItem;
+	
+	
+	private SQLiteDatabase db;
+	private DevOpenHelper helper;
+	private DaoSession daoSession;
+	private DaoMaster daoMaster;
+	
+	SzabadAutoListFragment szabadAutoListFragment;
+	SzabadAutoListFragment sajatAutoFragment;
+	
+
+	Tab szabadAutokTab;
+	Tab sajatAutoTab;
+
+	ActionBar actionBar;
+	CarsPagerAdapter carsAdapter;
+	ViewPager carsPager;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        dataBaseInit();
+        
+        szabadAutoListFragment = new SzabadAutoListFragment(this, autoDao);
+        sajatAutoFragment = new SzabadAutoListFragment(this, autoDao);
+        
+        actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		carsPager = (ViewPager) findViewById(R.id.carsPager);
+		carsAdapter = new CarsPagerAdapter(getSupportFragmentManager());
+		carsPager.setAdapter(carsAdapter);
+		carsPager.setCurrentItem(0);
+		
+		szabadAutokTab = actionBar.newTab().setText(R.string.title_cars_section1);
+		szabadAutokTab.setTabListener(this);
+		actionBar.addTab(szabadAutokTab);
+
+		sajatAutoTab = actionBar.newTab().setText(R.string.title_cars_section2);
+		sajatAutoTab.setTabListener(this);
+		actionBar.addTab(sajatAutoTab);
+		
+		
+		carsPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// TODO Auto-generated method stub
+				getActionBar().setSelectedNavigationItem(position);
+
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
     }
 
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+	
+	
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_car, menu);
@@ -32,4 +134,61 @@ public class CarActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		carsPager.setCurrentItem(arg0.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	public class CarsPagerAdapter extends FragmentPagerAdapter {
+
+		public CarsPagerAdapter(FragmentManager fm) {
+			super(fm);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			// TODO Auto-generated method stub
+			switch (position) {
+				case 1 :
+					return new SzabadAutoListFragment(CarActivity.this, autoDao);
+				default : 
+					return new SzabadAutoListFragment(CarActivity.this, autoDao);
+			}
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 2;
+		}
+
+	}
+	
+	
+	public void dataBaseInit() {
+		helper = new DaoMaster.DevOpenHelper(this, "flotta-db", null);
+		db = helper.getWritableDatabase();
+		daoMaster = new DaoMaster(db);
+		daoSession = daoMaster.newSession();
+
+		autoDao = (daoSession.getAutoDao());     
+		autoKepDao = daoSession.getAutoKepDao();
+	}
+	
 }
---
+++ FlottaKezelo/src/com/schonherz/fragments/SzabadAutoListFragment.java
@@ -0,0 +1,564 @@
+package com.schonherz.fragments;
+
+import java.text.Collator;
+import java.text.ParseException;
+import java.text.RuleBasedCollator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.Toast;
+
+import com.schonherz.adapters.AutoAdapter;
+import com.schonherz.classes.JsonArrayToArrayList;
+import com.schonherz.classes.JsonFromUrl;
+import com.schonherz.classes.NetworkUtil;
+import com.schonherz.classes.PullToRefreshListView;
+import com.schonherz.classes.PullToRefreshListView.OnRefreshListener;
+import com.schonherz.dbentities.Auto;
+import com.schonherz.dbentities.AutoDao;
+import com.schonherz.dbentities.SoforDao.Properties;
+import com.schonherz.flottadroid.R;
+
+public class SzabadAutoListFragment extends Fragment {
+
+	Context context;
+	AutoDao autoDao;
+	AutoAdapter adapter;
+	PullToRefreshListView pullListView;
+	ArrayList<Auto> autok;
+	MenuItem refreshItem;
+
+	public SzabadAutoListFragment(Context context, AutoDao autoDao) {
+		this.context = context;
+		this.autoDao = autoDao;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		setHasOptionsMenu(true);
+		super.onCreate(savedInstanceState);
+
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		// TODO Auto-generated method stub
+		inflater.inflate(R.menu.fragment_list_menu, menu);
+		SearchView searchView = (SearchView) menu.findItem(R.id.menu_search)
+				.getActionView();
+		setupSearchView(searchView);
+		refreshItem = menu.findItem(R.id.menu_refresh);
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+
+	public void setupSearchView(SearchView searchView) {
+		SearchManager searchManager = (SearchManager) context
+				.getSystemService(Context.SEARCH_SERVICE);
+		searchView.setSearchableInfo(searchManager
+				.getSearchableInfo(getActivity().getComponentName()));
+		searchView.setIconifiedByDefault(false);
+		searchView.setSubmitButtonEnabled(true);
+		searchView.setOnQueryTextListener(new OnQueryTextListener() {
+
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				// TODO Auto-generated method stub
+				if (query.length() > 0) {
+					ArrayList<Auto> templist = new ArrayList<Auto>();
+
+					for (int i = 0; i < autok.size(); i++) {
+						if (autok.get(i).getAutoNev().toLowerCase()
+								.contains(query.toLowerCase()))
+
+						{
+							templist.add(autok.get(i));
+
+						}
+					}
+
+					adapter.clear();
+					adapter.addAll(templist);
+					adapter.notifyDataSetChanged();
+				} else {
+					adapter.clear();
+					adapter.addAll(autok);
+					adapter.notifyDataSetChanged();
+				}
+				return true;
+			}
+
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				// TODO Auto-generated method stub
+
+				ArrayList<Auto> templist = new ArrayList<Auto>();
+
+				for (int i = 0; i < autok.size(); i++) {
+					if (autok.get(i).getAutoNev().toLowerCase()
+							.contains(newText.toLowerCase()))
+
+					{
+						templist.add(autok.get(i));
+
+					}
+				}
+
+				adapter.clear();
+				adapter.addAll(templist);
+				adapter.notifyDataSetChanged();
+
+				return true;
+			}
+		});
+
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View v = inflater.inflate(R.layout.layout_pulltorefresh_list, null);
+
+		pullListView = (PullToRefreshListView) v
+				.findViewById(R.id.pulltorefresh_listview);
+
+		autok = new ArrayList<Auto>(autoDao.queryBuilder().where(com.schonherz.dbentities.AutoDao.Properties.AutoFoglalt.eq(true)).list());
+
+		adapter = new AutoAdapter(context, R.layout.list_item_auto, autok,
+				autoDao);
+
+		pullListView.setAdapter(adapter);
+		pullListView.setOnRefreshListener(new OnRefreshListener() {
+
+			@Override
+			public void onRefresh() {
+				// TODO Auto-generated method stub
+				if (NetworkUtil.checkInternetIsActive(context)) {
+					new AsyncTask<Void, Void, Boolean>() {
+
+						@Override
+						protected void onPreExecute() {
+							startRefreshAnimation();
+						};
+
+						@Override
+						protected void onPostExecute(Boolean result) {
+							// TODO Auto-generated method stub
+							if (result == true) {
+								Toast.makeText(context, R.string.refreshed,
+										Toast.LENGTH_SHORT).show();
+
+							} else {
+								Toast.makeText(context, R.string.errorRefresh,
+										Toast.LENGTH_SHORT).show();
+							}
+
+							try {
+								// Play notification sound when refresn finished
+								Uri notification = RingtoneManager
+										.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+								Ringtone r = RingtoneManager.getRingtone(
+										context, notification);
+								r.play();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+
+							pullListView.onRefreshComplete();
+
+							adapter.clear();
+
+							ArrayList<Auto> autok = new ArrayList<Auto>(autoDao.queryBuilder().where(com.schonherz.dbentities.AutoDao.Properties.AutoFoglalt.eq(true)).list());
+
+							adapter.addAll(autok);
+
+							adapter.notifyDataSetChanged();
+
+							if (refreshItem != null
+									&& refreshItem.getActionView() != null) {
+								refreshItem.getActionView().clearAnimation();
+								refreshItem.setActionView(null);
+							}
+
+							stopRefreshAnimation();
+						}
+
+						@Override
+						protected Boolean doInBackground(Void... params) {
+							// TODO Auto-generated method stub
+							return saveAutoTable();
+						}
+
+					}.execute();
+				}
+
+				else {
+					Toast.makeText(context, R.string.no_internet,
+							Toast.LENGTH_SHORT).show();
+					pullListView.onRefreshComplete();
+				}
+			}
+		});
+
+		return v;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+			case R.id.menu_Sort :
+				AlertDialog.Builder builder = new AlertDialog.Builder(context);
+				builder.setTitle("Rendezés");
+				final CharSequence[] choiceList = {"Foglalt", "Név", "Típus",
+						"Rendszám", "Kilóméter óra", "Mûszaki vizsga idõ",
+						"Szervíz idõ", "Telephely név"};
+
+				int selected = -1; // does not select anything
+
+				builder.setSingleChoiceItems(choiceList, selected,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {							
+								switch (which) {
+									case 0 :
+										Collections.sort(autok,
+												new Comparator<Auto>() {
+													public int compare(
+															Auto lhs, Auto rhs) {
+														return lhs
+																.getAutoFoglalt()
+																.compareTo(
+																		rhs.getAutoFoglalt());
+													};
+												});
+										adapter.clear();
+										adapter.addAll(autok);
+										adapter.notifyDataSetChanged();
+										break;
+									case 1 :
+										;
+										try {
+											RuleBasedCollator huCollator = new RuleBasedCollator(
+													hungarianRules);
+											sortAutoNev(huCollator, autok);
+											adapter.clear();
+											adapter.addAll(autok);
+											adapter.notifyDataSetChanged();
+										} catch (ParseException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
+										break;
+									case 2 :
+
+										try {
+											RuleBasedCollator huCollator = new RuleBasedCollator(
+													hungarianRules);
+											sortAutoTipus(huCollator, autok);
+											adapter.clear();
+											adapter.addAll(autok);
+											adapter.notifyDataSetChanged();
+										} catch (ParseException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
+										break;
+									case 3 :
+										try {
+											RuleBasedCollator huCollator = new RuleBasedCollator(
+													hungarianRules);
+											sortAutoRendszam(huCollator, autok);
+											adapter.clear();
+											adapter.addAll(autok);
+											adapter.notifyDataSetChanged();
+										} catch (ParseException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
+										break;
+									case 4 :
+										Collections.sort(autok,
+												new Comparator<Auto>() {
+													public int compare(
+															Auto lhs, Auto rhs) {
+														return lhs
+																.getAutoKilometerOra()
+																.compareTo(
+																		rhs.getAutoKilometerOra());
+													};
+												});
+										adapter.clear();
+										adapter.addAll(autok);
+										adapter.notifyDataSetChanged();
+										break;
+									case 5 :
+										List<Auto> temp = autoDao
+												.queryBuilder()
+												.where(com.schonherz.dbentities.AutoDao.Properties.AutoFoglalt.eq(true))
+												.orderDesc(
+														com.schonherz.dbentities.AutoDao.Properties.AutoMuszakiVizsgaDate)
+												.list();
+										autok.clear();
+										autok.addAll(temp);
+										adapter.clear();
+										adapter.addAll(autok);
+										adapter.notifyDataSetChanged();										
+										break;
+									case 6 :
+										List<Auto> temp2 = autoDao
+										.queryBuilder()
+										.where(com.schonherz.dbentities.AutoDao.Properties.AutoFoglalt.eq(true))
+										.orderDesc(
+												com.schonherz.dbentities.AutoDao.Properties.AutoLastSzervizDate)
+										.list();
+										autok.clear();
+										autok.addAll(temp2);
+										adapter.clear();
+										adapter.addAll(autok);
+										adapter.notifyDataSetChanged();	
+										break;
+									case 7 :
+										try {
+											RuleBasedCollator huCollator = new RuleBasedCollator(
+													hungarianRules);
+											sortAutoTelephelyNev(huCollator, autok);
+											adapter.clear();
+											adapter.addAll(autok);
+											adapter.notifyDataSetChanged();
+										} catch (ParseException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
+										break;
+								}
+
+								dialog.dismiss();
+							}
+						});
+				AlertDialog alert = builder.create();
+				alert.show();
+
+				break;
+			case R.id.menu_refresh :
+				if (NetworkUtil.checkInternetIsActive(context) == true) {
+					new AsyncTask<Void, Void, Boolean>() {
+
+						@Override
+						protected void onPreExecute() {
+							startRefreshAnimation();
+						};
+
+						@Override
+						protected void onPostExecute(Boolean result) {
+							// TODO Auto-generated method stub
+							if (result == true) {
+								Toast.makeText(context, R.string.refreshed,
+										Toast.LENGTH_SHORT).show();
+
+							} else {
+								Toast.makeText(context, R.string.errorRefresh,
+										Toast.LENGTH_SHORT).show();
+							}
+
+							try {
+								// Play notification sound when refresn finished
+								Uri notification = RingtoneManager
+										.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+								Ringtone r = RingtoneManager.getRingtone(
+										context, notification);
+								r.play();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+
+							adapter.clear();
+
+							ArrayList<Auto> autok = new ArrayList<Auto>(autoDao.queryBuilder().where(com.schonherz.dbentities.AutoDao.Properties.AutoFoglalt.eq(true)).list());
+
+							adapter.addAll(autok);
+
+							adapter.notifyDataSetChanged();
+
+							if (refreshItem != null
+									&& refreshItem.getActionView() != null) {
+								refreshItem.getActionView().clearAnimation();
+								refreshItem.setActionView(null);
+							}
+
+							stopRefreshAnimation();
+						}
+
+						@Override
+						protected Boolean doInBackground(Void... params) {
+							// TODO Auto-generated method stub
+							return saveAutoTable();
+						}
+
+					}.execute();
+				} else {
+					Toast.makeText(context, R.string.no_internet,
+							Toast.LENGTH_SHORT).show();
+				}
+				break;
+
+		}
+
+		return true;
+	}
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		adapter.notifyDataSetChanged();
+		super.onResume();
+	}
+
+	public Boolean saveAutoTable() {
+		JSONArray jsonArray;
+		JSONObject json;
+
+		String serverAddres = "http://www.flotta.host-ed.me/queryAutoTable.php";
+
+		json = new JSONObject();
+
+		try {
+			jsonArray = (JSONArray) JsonFromUrl.getJsonObjectFromUrl(
+					serverAddres, json.toString());
+
+			// Eldobjuk a tablat es ujra letrehozzuk
+			autoDao.dropTable(autoDao.getDatabase(), true);
+			autoDao.createTable(autoDao.getDatabase(), true);
+
+			ArrayList<Auto> autok = JsonArrayToArrayList
+					.JsonArrayToAuto(jsonArray);
+
+			for (int i = 0; i < autok.size(); i++) {
+				autoDao.insert(autok.get(i));
+			}
+
+			Log.w("autolistsize", Integer.toString(autoDao.loadAll().size()));
+
+			return true;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private void stopRefreshAnimation() {
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		ImageView iv = (ImageView) inflater.inflate(R.layout.refreshing_layout,
+				null);
+	}
+
+	private void startRefreshAnimation() {
+
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		ImageView iv = (ImageView) inflater.inflate(R.layout.refreshing_layout,
+				null);
+		Animation rotation = AnimationUtils.loadAnimation(context,
+				R.anim.refresh_rotate);
+		rotation.setRepeatCount(Animation.INFINITE);
+		iv.startAnimation(rotation);
+		refreshItem.setActionView(iv);
+	}
+
+	// This code here for Hungarian String comparation
+
+	String hungarianRules = ("< a,A < á,Á < b,B < c,C < cs,Cs < d,D < dz,Dz < dzs,Dzs "
+			+ "< e,E < é,É < f,F < g,G < gy,Gy < h,H < i,I < í,Í < j,J "
+			+ "< k,K < l,L < ly,Ly < m,M < n,N < ny,Ny < o,O < ó,Ó "
+			+ "< ö,Ö < õ,Õ < p,P < q,Q < r,R < s,S < sz,Sz < t,T "
+			+ "< ty,Ty < u,U < ú,Ú < ü,Ü < û,Û < v,V < w,W < x,X < y,Y < z,Z < zs,Zs");
+
+
+	public static void sortAutoNev(Collator collator, List<Auto> autoList) {
+		Auto temp;
+		for (int i = 0; i < autoList.size(); i++) {
+			for (int j = 0; j < autoList.size(); j++) {
+				if (collator.compare(autoList.get(j).getAutoNev(), autoList
+						.get(i).getAutoNev()) > 0) {
+					temp = autoList.get(i);
+					autoList.set(i, autoList.get(j));
+					autoList.set(j, temp);
+				}
+			}
+		}
+	}
+
+	public static void sortAutoTipus(Collator collator, List<Auto> autoList) {
+		Auto temp;
+		for (int i = 0; i < autoList.size(); i++) {
+			for (int j = 0; j < autoList.size(); j++) {
+				if (collator.compare(autoList.get(j).getAutoTipus(), autoList
+						.get(i).getAutoTipus()) > 0) {
+					temp = autoList.get(i);
+					autoList.set(i, autoList.get(j));
+					autoList.set(j, temp);
+				}
+			}
+		}
+	}
+
+	public static void sortAutoRendszam(Collator collator, List<Auto> autoList) {
+		Auto temp;
+		for (int i = 0; i < autoList.size(); i++) {
+			for (int j = 0; j < autoList.size(); j++) {
+				if (collator.compare(autoList.get(j).getAutoRendszam(),
+						autoList.get(i).getAutoRendszam()) > 0) {
+					temp = autoList.get(i);
+					autoList.set(i, autoList.get(j));
+					autoList.set(j, temp);
+				}
+			}
+		}
+	}
+	
+	public static void sortAutoTelephelyNev(Collator collator, List<Auto> autoList) {
+		Auto temp;
+		for (int i = 0; i < autoList.size(); i++) {
+			for (int j = 0; j < autoList.size(); j++) {
+				if (collator.compare(autoList.get(i).getTelephely().getTelephelyNev(),
+						autoList.get(j).getTelephely().getTelephelyNev()) > 0) {
+					temp = autoList.get(i);
+					autoList.set(i, autoList.get(j));
+					autoList.set(j, temp);
+				}
+			}
+		}
+	}
+
+}
