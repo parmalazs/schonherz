54c699f4d3a8d28459291450a52fb5137df490c8
Geza <csrbgeza@gmail.com>
2013-05-27 01:48:25 +0200

    csunya, de majd meg csiszolom a heten

--- FlottaKezelo/src/com/schonherz/adapters/MunkaAdapter.java
+++ FlottaKezelo/src/com/schonherz/adapters/MunkaAdapter.java
@@ -1,9 +1,11 @@
 package com.schonherz.adapters;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import com.schonherz.adapters.PartnerAdapter.ViewHolder;
 import com.schonherz.dbentities.Munka;
 import com.schonherz.dbentities.MunkaDao;
 
@@ -12,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MunkaAdapter extends ArrayAdapter<Munka> {
@@ -88,22 +91,90 @@ public class MunkaAdapter extends ArrayAdapter<Munka> {
 		ViewHolder holder=null;
 		Munka currentMunka=munkak.get(position);
 		if(convertView==null) {
-			LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			convertView = inflater.inflate(com.schonherz.flottadroid.R.layout.list_item_munka, null);
-			
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(
+					com.schonherz.flottadroid.R.layout.list_item_munka, null);
 			holder = new ViewHolder();
-			holder.munkaNevTextView=(TextView)convertView.findViewById(com.schonherz.flottadroid.R.id.textViewMunkaNev);
-	
-			holder.munkaNevTextView.setText(currentMunka.getMunkaDate());
+
+			// holder.partnerNevTextView =
+			// (TextView)convertView.findViewById(com.schonherz.flottadroid.R.id.textViewPartnerNev);
+			// holder.partnerNevTextView.setText(currentPartner.getPartnerNev());
+
+			// set avatar
+			// ImageView ivAvatar =
+			// ivAvatar.setImageBitmap(Bitmap.createBitmap(currentPartner.getPartnerKepList().get(0).getPartnerKepPath()));
+
+			ImageView iv = (ImageView) convertView
+					.findViewById(com.schonherz.flottadroid.R.id.imgAvatar);
+			// Bitmap myBitmap =
+			// BitmapFactory.decodeFile(currentPartner.getPartnerKepList().get(0).getPartnerKepPath());
+
+			// Image should be scaled somehow
+			// iv.setScaleType(ImageView.ScaleType.CENTER);
+			// iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			// iv.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+			// iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+			// iv.setScaleType(ImageView.ScaleType.FIT_XY);
+			// iv.setScaleType(ImageView.ScaleType.FIT_END);
+			// iv.setBackgroundColor(Color.BLACK);
+			// Set the Width & Height of the individual images
+			// iv.setLayoutParams(new Gallery.LayoutParams(130, 180));
+
+			// set name
+			TextView tvName = (TextView) convertView
+					.findViewById(com.schonherz.flottadroid.R.id.tvMunkaDatum);
+			tvName.setText(currentMunka.getMunkaDate());
+			// set phone
+			TextView tvPhone = (TextView) convertView
+					.findViewById(com.schonherz.flottadroid.R.id.tvMunkaTelephelye);
+			tvPhone.setText(currentMunka.getTelephely().getTelephelyNev());
+			// set email
+			TextView tvEmail = (TextView) convertView
+					.findViewById(com.schonherz.flottadroid.R.id.tvMunkaBecsultIdoOraban);
+			tvEmail.setText((new DecimalFormat("##.##").format(currentMunka.getMunkaEstimatedTime()/60.0))+" h");
+			
+			TextView tvLast = (TextView) convertView.findViewById(com.schonherz.flottadroid.R.id.tvMunkaBevetel);
+			tvLast.setText((new DecimalFormat("##.##").format(currentMunka.getMunkaBevetel())) + " $");
+			
+			
 		}
 		else {
 			holder=(ViewHolder)convertView.getTag();
 		}
 		
 		munkaDao.refresh(currentMunka);
-		holder=new ViewHolder();
-		holder.munkaNevTextView=(TextView)convertView.findViewById(com.schonherz.flottadroid.R.id.textViewMunkaNev);
-		holder.munkaNevTextView.setText(currentMunka.getMunkaDate());
+		ImageView iv = (ImageView) convertView
+				.findViewById(com.schonherz.flottadroid.R.id.imgAvatar);
+		// Bitmap myBitmap =
+		// BitmapFactory.decodeFile(currentPartner.getPartnerKepList().get(0).getPartnerKepPath());
+
+		// Image should be scaled somehow
+		// iv.setScaleType(ImageView.ScaleType.CENTER);
+		// iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+		// iv.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+		// iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+		// iv.setScaleType(ImageView.ScaleType.FIT_XY);
+		// iv.setScaleType(ImageView.ScaleType.FIT_END);
+		// iv.setBackgroundColor(Color.BLACK);
+		// Set the Width & Height of the individual images
+		// iv.setLayoutParams(new Gallery.LayoutParams(130, 180));
+
+		// set name
+		TextView tvName = (TextView) convertView
+				.findViewById(com.schonherz.flottadroid.R.id.tvMunkaDatum);
+		tvName.setText(currentMunka.getMunkaDate());
+		// set phone
+		TextView tvPhone = (TextView) convertView
+				.findViewById(com.schonherz.flottadroid.R.id.tvMunkaTelephelye);
+		tvPhone.setText(currentMunka.getTelephely().getTelephelyNev());
+		// set email
+		TextView tvEmail = (TextView) convertView
+				.findViewById(com.schonherz.flottadroid.R.id.tvMunkaBecsultIdoOraban);
+		tvEmail.setText((new DecimalFormat("##.##").format(currentMunka.getMunkaEstimatedTime()/60.0))+" h");
+		
+		TextView tvLast = (TextView) convertView.findViewById(com.schonherz.flottadroid.R.id.tvMunkaBevetel);
+		tvLast.setText((new DecimalFormat("##.##").format(currentMunka.getMunkaBevetel())) + " $");
 		
 		return convertView;
 	}
