a8ca4c9b3a7200fd0cb8b16c0e2f71a6a459c439
Gyöngyösi Tamás <gytomi@gmail.com>
2013-05-29 11:45:28 +0200

    notification

--- FlottaKezelo/src/com/schonherz/flottadroid/C2DMMessageReceiver.java
+++ FlottaKezelo/src/com/schonherz/flottadroid/C2DMMessageReceiver.java
@@ -1,6 +1,9 @@
 package com.schonherz.flottadroid;
 
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -19,13 +22,31 @@ public class C2DMMessageReceiver extends BroadcastReceiver{
 		      Log.w("C2DM", "Received message");
 		      final String payload = intent.getStringExtra("message");
 		      Log.d("C2DM", "dmControl: payload = " + payload);
-		      // Send this to my application server
+		   // Send this to my application server
+		      /*
+		      Intent i = new Intent();
+		      i.putExtra("message",intent.getStringExtra("message"));
+		      i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		      i.setClass(context, AlertActivity.class);
+		      context.startActivity(i);*/
 		      
-		     Intent i = new Intent();
-		     i.putExtra("message",intent.getStringExtra("message"));
-		     i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		     i.setClass(context, AlertActivity.class);
-		     context.startActivity(i);
+		     
+		      // Prepare intent which is triggered if the
+		      // notification is selected
+		      Intent notificationIntent = new Intent(context, AlertActivity.class);
+		      notificationIntent.putExtra("message", intent.getStringExtra("message"));
+		      PendingIntent pIntent = PendingIntent.getActivity(context, 0, notificationIntent,  PendingIntent.FLAG_UPDATE_CURRENT);
+
+		      // Build notification
+		      // Actions are just fake
+		      Notification.Builder noti = new Notification.Builder(context)
+		          .setContentTitle("Üzenet via FlottaDroid")
+		          .setContentText(intent.getStringExtra("message")).setSmallIcon(R.drawable.saroklogo)
+		          .setContentIntent(pIntent);
+		      NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+		      noti.getNotification().flags |= Notification.FLAG_AUTO_CANCEL;
+
+		      notificationManager.notify(0, noti.getNotification());
 		    }
 	}
 
